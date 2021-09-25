1070e7b1c8ccbe311ef19fb54576d194c10d917b
wanjugucode <karobiaanastasia.com>
2021-09-25 10:55:44 +0300

    modified files

--- Core/views.py
+++ Core/views.py
@@ -7,10 +7,10 @@ from event.models import Event
 
 # Create your views here.
 def home(request):
-    students=Student.objects.count(),
-    trainers=Trainer.objects.count(),
-    courses=Course.objects.count(),
-    events=Event.objects.count(),
+    students=Student.objects.count()
+    trainers=Trainer.objects.count()
+    courses=Course.objects.count()
+    events=Event.objects.count()
     data={'student':students,'trainer':trainers,'course':courses,'event':events}
     return render(request,'home.html',data)
    
